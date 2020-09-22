1a163afccf9b00d68850fb58fe6eb59171a2fce0
zied <medziedrahmani@yahoo.com>
2020-09-22 06:44:33 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import InputTodo from "./components/InputTodo";
+
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 0,isCompleted:false, text: "zied" },
+    { id: 3,isCompleted:false, text: "zied2" },
+    { id: 2,isCompleted:false, text: "zied3" },
+  ]);
+  // 111111
+  const handleAdd = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const handleDelete = (index) => {
+    setTodos(todos.filter((todo) => todo.id !== index));
+  };
+
+  const handleComplete=(id)=>{
+    setTodos(todos.map(todo=>
+        todo.id===id? {...todo,isCompleted:!todo.isCompleted}  : todo
+      ))
+  }
+const handleEdit=(id,editedText)=>{
+  setTodos(todos.map(todo=> todo.id===id ? {...todo,text:editedText} : todo ))
+}
+
+  return (
+    <div className="App">
+      {/* 222222 */}
+      <InputTodo handleAdd={handleAdd} />
+      <TodoList todos={todos} handleDelete={handleDelete} handleComplete={handleComplete} handleEdit={handleEdit} />
+    </div>
+  );
+}
+
+export default App;
